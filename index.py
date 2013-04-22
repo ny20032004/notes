afe45ce8f35ca247f1375000c1593b64b315ac10
shonenada <shonenada@gmail.com>
2013-04-22 19:45:00 +0800

    Added a python program to visit markdown files in a website.

---
+++ index.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import os
+import re
+
+import markdown2
+from bottle import route, error, run, static_file, redirect
+
+
+@error(404)
+def not_found(file_name):
+    return file_name + " not found in this web."
+
+
+def list_md_files(folder=None):
+    """ List the *.md files order by Modified time"""
+    if folder:
+        path = './md/' + folder + '/'
+    else:
+        path = './md/'
+    md_files = []
+    if not os.path.exists(path):
+        redirect("/")
+    files = [(os.path.getmtime(path + x), x)
+             for x in os.listdir(path)]
+    files.sort()
+    files.reverse()
+    type_pattern = re.compile("\.md$" , re.I)
+    for mtime, mdfile in files:
+        if type_pattern.search(mdfile):
+            md_files.append(mdfile.replace(".md", ""))
+    return md_files
+
+
+def render_md(md_content, md_name):
+    """Render markdown files"""
+    html = ("<html><head><title>" + md_name + "</title><meta http-equiv"
+            "=\"Content-Type\" content=\"text/html; charset=utf-8\" />"
+            "<link rel=\"stylesheet\" href=\"/static/styles/page.css\" "
+            "type=\"text/css\" />"
+            "<link rel=\"stylesheet\" href=\"/static/styles/github.css\" "
+            "type=\"text/css\" />"
+            "</head><body><div id=\"main\" "
+            "class=\"markdown-body entry-content\">")
+    html += markdown2.markdown(md_content)
+    html += "</div></body></html>"
+    return html
+
+
+def read_md(file_name):
+    if not os.path.exists("./md/" + file_name + '.md'):
+        redirect("/" + file_name + "/")
+    md_file = open("./md/" + file_name + '.md', 'r')
+    if md_file:
+        html = md_file.read()
+    html += "\n\n\n[*<<<*Back](/)\n\n\n"
+    return render_md(html, file_name)
+
+
+def generate_index(folder=None):
+    html = "# Index\n"
+    md_files = list_md_files(folder)
+    for md_file in md_files:
+        html += " 1. [" + md_file + "](/" + md_file + ")\n"
+    return render_md(html, 'Index')
+
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='./static')
+
+
+@route('/')
+@route('/<folder:path>/')
+def home(folder=None):
+    if not folder:
+        index_path = 'Home'
+    else:
+        index_path = folder + '/Home'
+    if os.path.exists('./md/' + index_path + '.md'):
+        html = read_md(index_path)
+    else:
+        html = generate_index(folder)
+    return html
+
+
+@route('/<page>')
+@route('/<folder:path>/<page>')
+def page(page, folder=None):
+    if not folder:
+        html = read_md(page)
+    else:
+        html = read_md(folder + '/' + page)
+    return html
+
+
+run(host='localhost', port=8888, debug=True)
--- md/Compare between performance of C++ raw Point and Smart Point.md
+++ md/Compare between performance of C++ raw Point and Smart Point.md
@@ -6,20 +6,22 @@
 我在 [Similar Query and Index in C++](http://bitbucket.org/shonenada/sqai-in-cpp)项目中使用了智能指针。为了了解使用智能指针的性能消耗，我在这项目里进行了性能测试。
 
 ### 使用内置指针
-
     CDNA* temp = new CDNA(ident, currentSquence);
+
 * 在 maxSize = 500,000 的时候，执行平均总时间：4.095s (4.11s + 4.08s).
 
 * 在 maxSize = 1,000,000 的时候，执行平均总时间：10.67s (10.62s + 10.72s)
 
 ### 使用智能指针
     shared_ptr<CDNA> temp(new CDNA(ident, currentSquence));
+
 * 在 maxSize = 500,000 的时候，执行平均总时间：4.235s (4.29s + 4.18s)
 
 * 在 maxSize = 1,000,000 的时候，执行平均总时间：10.95s (11.01s + 10.89s)
 
 ### 使用引用（对象拷贝
     CDNA temp(ident, currentSquence);
+
 * 在 maxSize = 500,000 的时候，执行总时间：4.78s
  
 * 在 maxSize = 1,000,000 的时候，执行平均总时间：12.29s
